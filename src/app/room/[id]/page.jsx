d65f381e46f1e7aa40c67d0f00b0daee729eeb67
Ayush Mehrotra <ayushmehrotra@gmail.com>
2023-07-12 17:35:21 +0530

    left to add UI

---
+++ src/app/room/[id]/page.jsx
@@ -0,0 +1,39 @@
+"use client"
+import React from "react";
+import {ZegoUIKitPrebuilt} from "@zegocloud/zego-uikit-prebuilt"
+
+export default function roomPage({params}){
+    const roomId = params.id
+    const myMeeting = async (element)=>{
+        const appID = 890409462;
+        const serverSecret = '54cf083f176efdb6b10e4224b84ed2dc';
+        const kitToken  =
+         ZegoUIKitPrebuilt.generateKitTokenForTest(appID, serverSecret,
+            roomId,
+            Date.now().toString(),
+            "Main User"
+            );
+
+            const zc = ZegoUIKitPrebuilt.create(kitToken)
+            zc.joinRoom({
+                container:element,
+                sharedLinks: [{
+                        name:"copy link",
+                        url: `http://localhost:3000/room/${roomId}`
+                }],
+                
+                scenario:{
+                    mode:ZegoUIKitPrebuilt.OneONoneCall,
+                },
+                showScreenSharingButton:true,
+            })
+    }
+
+    return(
+        <>
+        <div>
+        <div ref={myMeeting} />
+        </div>
+        </>
+    )
+}
